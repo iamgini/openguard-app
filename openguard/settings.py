7e767660f2c5bbe1280fd7677b75fca5d9235d97
ginigangadharan <net.gini@gmail.com>
2022-05-08 17:46:04 +0800

    update

--- openguard/settings.py
+++ openguard/settings.py
@@ -178,6 +178,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+## Add
+CSRF_TRUSTED_ORIGINS = ["http://openguard.lab.local", "http://www.yourdomain.com"]
+
 ## Gineesh
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
